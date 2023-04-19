a9d12973ed59035d3858f0bf9c3cc40725bf2124
Chris <chrisfree1122@gmail.com>
2023-04-18 21:08:01 -0500

    Creacion de practicas

--- src/practicaIngeniero.java
+++ src/practicaIngeniero.java
@@ -1,10 +1,16 @@
 public class practicaIngeniero {
 
     public static void main(String[] args) {
+        // Obtencion del residuo de 0 = Es decir numeros impares
+
         for (int i = 1; i < 100; i++ ){
+
             if (i % 3 == 0)    {
+
                 System.out.println(i);
+
             }
         }
+
     }
 }
